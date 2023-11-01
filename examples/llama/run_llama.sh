b1c21fd28056fa9a7b0a7b76d1859ec272b969bb
Shu Anzai <wisteria1729@gmail.com>
2023-11-01 22:25:11 +0000

    script to easily execute llama

---
+++ examples/llama/run_llama.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename "$0")
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$( cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd )
+
+CONTAINER_NAME=tensorrt_llm-release-$USER
+
+# Usage: ./run_llama.sh [batch_size] [output_len]
+batch_size=${1-"1"}
+output_len=${2-"64"}
+
+if [ -f /.dockerenv ];
+then
+  echo "Inside docker"
+  pushd /TensorRT-LLM/examples/llama
+  python3 run.py --input_tokens ./inputs/input_"$batch_size".csv --max_output_len "$output_len" --tokenizer_dir meta-llama/Llama-2-7b-hf --engine_dir=./tmp/llama/7B/trt_engines/fp16/1-gpu/
+  popd
+else
+  echo "On host machine"
+  docker exec -it ${CONTAINER_NAME} bash -c "cd /TensorRT-LLM && ./examples/llama/${SCRIPT_NAME} $batch_size $output_len"
+fi
