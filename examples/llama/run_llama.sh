c24e1724c727c20683f554c4ab1b2a19b160684f
Shu Anzai <wisteria1729@gmail.com>
2023-11-02 01:06:36 +0000

    script for enabling running with mpirun

--- examples/llama/run_llama.sh
+++ examples/llama/run_llama.sh
@@ -6,17 +6,26 @@ ROOT_DIR=$( cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd )
 
 CONTAINER_NAME=tensorrt_llm-release-$USER
 
-# Usage: ./run_llama.sh [batch_size] [output_len]
+# Usage: ./run_llama.sh [batch_size] [output_len] [pipeline_num]
 batch_size=${1-"1"}
 output_len=${2-"64"}
+pipeline_num=${3:-1}
+
+command="python3 run.py --input_tokens ./inputs/input_${batch_size}.csv --max_output_len ${output_len} --tokenizer_dir meta-llama/Llama-2-7b-hf --engine_dir=./tmp/llama/7B/trt_engines/fp16/${pipeline_num}-gpu/"
 
 if [ -f /.dockerenv ];
 then
   echo "Inside docker"
   pushd /TensorRT-LLM/examples/llama
-  python3 run.py --input_tokens ./inputs/input_"$batch_size".csv --max_output_len "$output_len" --tokenizer_dir meta-llama/Llama-2-7b-hf --engine_dir=./tmp/llama/7B/trt_engines/fp16/1-gpu/
+  if [ ${pipeline_num} -gt 1 ]; then
+    echo "Runing mpirun -n ${pipeline_num} --allow-run-as-root ${command}"
+    mpirun -n ${pipeline_num} --allow-run-as-root ${command}
+  else
+    echo "Runing ${command}"
+    ${command}
+  fi
   popd
 else
   echo "On host machine"
-  docker exec -it ${CONTAINER_NAME} bash -c "cd /TensorRT-LLM && ./examples/llama/${SCRIPT_NAME} $batch_size $output_len"
+  docker exec -it ${CONTAINER_NAME} bash -c "cd /TensorRT-LLM && ./examples/llama/${SCRIPT_NAME} $batch_size $output_len $pipeline_num"
 fi
