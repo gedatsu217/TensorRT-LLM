41d3269bb596c07af8884231becf14c271c92201
Shu Anzai <wisteria1729@gmail.com>
2023-11-02 01:21:35 +0000

    llama eval script

---
+++ examples/llama/eval_llama.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$( cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd )
+
+
+batch_initial=1
+batch_max=128
+output_initial=1
+output_max=512
+
+for ((batch=batch_initial; batch<=batch_max; batch=batch*2))
+do
+    for ((output=output_initial; output<=output_max; output=output*2))
+    do
+        output_file="${ROOT_DIR}/tmp/a100x4/manifold_llama-"$batch"-"$output".log"
+        ${ROOT_DIR}/examples/llama/run_llama.sh ${batch} ${output} 4 > ${output_file}
+        echo "Done $batch $output"
+    done
+done
+
+echo "Done all the executions"
